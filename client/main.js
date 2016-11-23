851df138dd4f5f5f83c5f4fe353a1e6991d0f003
leon-t <l.todtenhausen@hotmail.de>
2016-11-23 13:05:59 +0100

    [GG-20] Replaced the sample application with a simple web site that contains just a map

--- client/main.js
+++ client/main.js
@@ -1,22 +1,18 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+// on startup run resizing event
+Meteor.startup(function() {
+  $(window).resize(function() {
+    $('#map').css('height', window.innerHeight - 82 - 45);
+  });
+  $(window).resize(); // trigger resize event 
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+Template.map.rendered = function() {
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+  var map = L.map('map').setView([52.504, 13.422], 10);
+  var tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: '',
+      maxZoom: 18,
+      id: 'leon4037.0bo1i1h7',
+      accessToken: 'pk.eyJ1IjoibGVvbjQwMzciLCJhIjoiY2lwODdrNWV5MDAzMHV0bm1oamxjejcyaCJ9.oQomGba208ZZcnOslt3D3g'
+  }).addTo(map);
+};
