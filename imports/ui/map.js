12bdaced8c36aebcf372708fa9a43c69b991f481
leon-t <l.todtenhausen@hotmail.de>
2016-12-07 17:14:20 +0100

    Datenbanksynchronisation zwischen Client und Server korrekt eingerichtet. Marker werden nun direkt beim Laden der Karte erstellt.

--- imports/ui/map.js
+++ imports/ui/map.js
@@ -3,43 +3,58 @@ import { Markers } from '../api/markers.js';
 
 import './map.html';
 
-Template.map.rendered = function() {
-  L.Icon.Default.imagePath = 'packages/bevanhunt_leaflet/images/';
+var markerLayer = L.layerGroup([]);
 
-  map = L.map('map').setView([52.504, 13.422], 10);
-  var tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      attribution: '',
-      maxZoom: 18,
-      id: 'leon4037.0bo1i1h7',
-      accessToken: 'pk.eyJ1IjoibGVvbjQwMzciLCJhIjoiY2lwODdrNWV5MDAzMHV0bm1oamxjejcyaCJ9.oQomGba208ZZcnOslt3D3g'
-  }).addTo(map);
+Template.map.rendered = function() {
+    initMap();
+    Meteor.subscribe("markers", {
+    onReady: function () { 
+        console.log("Subscription is ready! Current count of collection is: "+ Markers.find().count());
+        updateMapWithCollectionData();
+    },
+    onError: function () { console.log("onError", arguments); }
+    });
 };
 
-Template.map.events({
-  'click'(event) {
-        var markers = Markers.find({}).fetch();
-        if (Markers.find({}).count() == 0){
-            console.log("Empty, filling Mongo DB");
-            Markers.insert({
-                location: [52.456951, 13.526402], 
-                description: "<b>HTW Berlin</b><br>Campus Wilhelminenhof Gebäude C<br>Koordinaten: 52.456951, 13.526402"
-            });
-            Markers.insert({
-                location: [52.506322, 13.443618], 
-                description: "<b>Mercedes Benz Arena</b><br>Spielort von Alba Berlin & Eisbären Berlin<br>Koordinaten: 52.506322, 13.443618"
-            });
-            Markers.insert({
-                location: [52.507968, 13.337746], 
-                description: "<b>Zoologischer Garten</b><br>Hardenbergplatz 15, 10623 Berlin, Deutschland<br>Koordinaten: 52.507968, 13.337746"
-            });
-            console.log("Filled Mongo DB");
-            console.log(Markers.find({}).count());
-        }
-        else {
-            console.log("Found entities!!!!!!!!!!!!");
-            markers.forEach(function(marker){
-              var newMarker = L.marker(marker.location).bindPopup(marker.description).addTo(map);
-            });
-        }
-  },
-});
+function initMap(){
+    L.Icon.Default.imagePath = 'packages/bevanhunt_leaflet/images/';
+
+    map = L.map('map').setView([52.504, 13.422], 10);
+    var tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: '',
+        maxZoom: 18,
+        id: 'leon4037.0bo1i1h7',
+        accessToken: 'pk.eyJ1IjoibGVvbjQwMzciLCJhIjoiY2lwODdrNWV5MDAzMHV0bm1oamxjejcyaCJ9.oQomGba208ZZcnOslt3D3g'
+    }).addTo(map);
+}
+
+function updateMapWithCollectionData(){
+    markerLayer.clearLayers(); // delete all existing markers
+    if (Markers.find({}).count() == 0){
+        console.log("Empty, filling Mongo DB");
+        Markers.insert({
+            location: [52.456951, 13.526402], 
+            description: "<b>HTW Berlin</b><br>Campus Wilhelminenhof Gebäude C<br>Koordinaten: 52.456951, 13.526402"
+        });
+        Markers.insert({
+            location: [52.506322, 13.443618], 
+            description: "<b>Mercedes Benz Arena</b><br>Spielort von Alba Berlin & Eisbären Berlin<br>Koordinaten: 52.506322, 13.443618"
+        });
+        Markers.insert({
+            location: [52.507968, 13.337746], 
+            description: "<b>Zoologischer Garten</b><br>Hardenbergplatz 15, 10623 Berlin, Deutschland<br>Koordinaten: 52.507968, 13.337746"
+        });
+        console.log("Filled Mongo DB");
+        console.log(Markers.find({}).count());
+    }
+    createMarkersOfCollectionEntities();
+}
+
+function createMarkersOfCollectionEntities(){
+    var markers = Markers.find({}).fetch();
+    console.log("Collection isn't empty! Adding " + Markers.find({}).count() + " entities to map...");
+    markers.forEach(function(marker){
+        var newMarker = L.marker(marker.location, {riseOnHover: true}).bindPopup(marker.description).addTo(markerLayer); // add new marker object for each marker entity in "Markers" collection
+    });
+    markerLayer.addTo(map); // add layer with added markers to map
+}
