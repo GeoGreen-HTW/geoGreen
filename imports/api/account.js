32b6722b9821371f5b4e6fe0856b7e010efade0e
TillBu <tillbussmann@googlemail.com>
2017-01-19 11:38:41 +0100

    cleaned up working directory

--- imports/api/account.js
+++
@@ -1,7 +0,0 @@
-import { Accounts } from 'meteor/accounts-base';
-
-/*
-Accounts.onCreateUser(function(options, user) {
-    _.extend(user, { username : 'init' }); 
-});
-*/
--- imports/ui/body.js
+++
@@ -1,5 +0,0 @@
-import { Template } from 'meteor/templating';
-import { Quests } from '../api/quests.js';
-
-import './map.js';
-import './body.html';
