9b3e18b6a7b1f6dc06430d8ef53c23479c89d23e
leon-t <l.todtenhausen@hotmail.de>
2017-01-23 10:53:20 +0100

    Erster verusch Bilder zu speichern. Probleme bei der Speicherung von ImageIDs in Questobjekten aufgrund von Problemen mit asynchronen Funktionsaufrufen. Darstellung der Bilder funktioniert auch nicht.

--- imports/ui/createPopup.js
+++ imports/ui/createPopup.js
@@ -1,5 +1,6 @@
   import './createPopup.html';
   import { Session } from 'meteor/session';
+  import { Images } from '../api/images.js';
 
   
   Template.createPopup.helpers({
@@ -12,7 +13,18 @@
         'submit .createForm'(event) {
             // Prevent default browser form submit
             event.preventDefault();
-
+            
+            var files = event.target.myFileInput.files;
+            var fileId = "undefined";
+            for (var i = 0, ln = files.length; i < ln; i++) {
+            Images.insert(files[i], function (err, fileObj) {
+                // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+                console.log(fileObj._id);
+                fileId = fileObj._id;
+                console.log(new Date().getTime());
+            });
+            }
+            console.log(new Date().getTime());
             var newQuest = { 
                 location: [m_lat, m_long], 
                 title : document.getElementById('title').value,
@@ -23,6 +35,7 @@
                 Zeiteinheit:document.getElementById('zeiteinheit').value,
                 Status: "offen",
                 Kosten:document.getElementById('kosten').value,
+                imageId: fileId
             };
 
             Meteor.call('quests.insert', newQuest);
@@ -30,4 +43,13 @@
             map.closePopup();
             console.log("Quest created!");
         },
+
+        // 'change .myFileInput': function(event, template) {
+        //     var files = event.target.files;
+        //     for (var i = 0, ln = files.length; i < ln; i++) {
+        //     Images.insert(files[i], function (err, fileObj) {
+        //         // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+        //     });
+        //     }
+        // },
     });
